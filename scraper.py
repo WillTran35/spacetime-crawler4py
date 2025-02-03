ee30b2ce8eab3a6ad02d5a02fc492dd90b4200b2
William Tran <willtran@Williams-MacBook-Pro.local>
2025-02-03 15:00:52 -0800

    finished extract hyperlinks from a webpage

--- scraper.py
+++ scraper.py
@@ -5,6 +5,11 @@ def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
+def extractLink(html : str):
+    # '<a href="https://example.com">Example</a> <a href="https://test.com">Test</a>'
+    pattern = r'href="(.*?)"'
+    return re.findall(pattern, html)
+
 def extract_next_links(url, resp):
     # Implementation required.
     # url: the URL that was used to get the page
@@ -15,7 +20,13 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+    # check resp.status, make sure it returns 200 before we crawl
+    # go thru resp.raw_response and look for <a> anchor tags
+    # convert to a string using resp.raw_response.content.decode("utf-8");
+
+    html = resp.raw_response.content.decode("utf-8")
+    return extractLink(html)
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
